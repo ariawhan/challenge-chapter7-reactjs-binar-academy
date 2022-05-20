1f037c68edddbcc252b36451cf504a4eae4c9e4d
Ariawhan <gede.aryawhan@gmail.com>
2022-05-20 16:02:56 +0800

    [Ariawhan] Update banner and add data banner

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,39 +1,38 @@
 import React, { Component } from "react";
+import banner from "../../data/banner.json";
 
 class Banner extends Component {
-  state = {};
+  state = {
+    title: banner.main.title,
+    description: banner.main.description,
+    btn: banner.main.btn,
+    img: banner.main.img,
+  };
   render() {
     return (
-      <div className="container mt-2">
-        <div className="header-banner">
-          <div className="row align-items-center intro">
-            <div className="col-md-6 intro-text">
-              <h1>
-                Sewa & Rental Mobil Terbaik di <br />
-                kawasan (Lokasimu)
-              </h1>
-              <p>
-                Selamat datang di Binar Car Rental. Kami menyediakan mobil
-                kualitas <br />
-                terbaik dengan harga terjangkau. Selalu siap melayani
-                kebutuhanmu <br />
-                untuk sewa mobil selama 24 jam.
-              </p>
-              <button
-                type="button"
-                className="button-global"
-                id="tombol"
-                onClick="search()"
-              >
-                Mulai Sewa Mobil
-              </button>
-            </div>
-            <div className="col-md-6 intro-img">
-              <img className="img-fluid" src="./images/img-car.svg" />
+      <>
+        <div className="container mt-2">
+          <div className="header-banner">
+            <div className="row align-items-center intro">
+              <div className="col-md-6 intro-text">
+                <h1>{this.state.title}</h1>
+                <p>{this.state.description}</p>
+                <button
+                  type="button"
+                  className="button-global"
+                  id="tombol"
+                  onClick="search()"
+                >
+                  {this.state.btn}
+                </button>
+              </div>
+              <div className="col-md-6 intro-img">
+                <img className="img-fluid" src={this.state.img} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
