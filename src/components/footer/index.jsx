10af1fdf67a189e4e2da0be8b355f698c42bf604
Ariawhan <gede.aryawhan@gmail.com>
2022-05-20 15:43:09 +0800

    [Ariawhan] Update components footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,93 +1,97 @@
 import React, { Component } from "react";
+import footer from "../../data/footer.json";
+import navbar from "../../data/navbar.json";
 
 class Footer extends Component {
-  state = {};
-  render() {
+  state = {
+    contact: footer.contact,
+    social: footer.social,
+    copyright: footer.copyright,
+    navigation: navbar,
+  };
+
+  contact = (contact) => {
     return (
-      <footer>
-        <div className="container mt-5">
+      <React.Fragment>
+        <p>{contact.address}</p>
+        <p>{contact.email}</p>
+        <p>{contact.number}</p>
+      </React.Fragment>
+    );
+  };
+
+  navigation = (navigation) => {
+    return (
+      <React.Fragment>
+        <div className="footer-nav">
+          {navigation.map((nav) => {
+            return (
+              <a href={nav.href}>
+                <p>{nav.name}</p>
+              </a>
+            );
+          })}
+        </div>
+      </React.Fragment>
+    );
+  };
+
+  socialMedia = (social) => {
+    return (
+      <React.Fragment>
+        <p>{social.title}</p>
+        <div className="footer-connect-icons">
           <div className="row">
-            <div className="col-lg-2 col-md-3 p-4">
-              <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
-              <p>binarcarrental@gmail.com</p>
-              <p>081-233-334-808</p>
-            </div>
-            <div className="ol-lg-2 col-md-3 p-4">
-              <div className="footer-nav">
-                <a href="#Services">
-                  <p>Our services</p>
-                </a>
-                <a href="#whyus">
-                  <p>Why Us</p>
-                </a>
-                <a href="#Testimonial">
-                  <p>Testimonial</p>
-                </a>
-                <a href="#faq">
-                  <p>FAQ</p>
-                </a>
-              </div>
-            </div>
-            <div className="ol-lg-2 col-md-3 p-4">
-              <p>Connect with us</p>
-              <div className="footer-connect-icons">
-                <div className="row">
-                  <div className="col-2">
-                    <a href="https://www.facebook.com/ariawhan">
-                      <img
-                        href="instagram.com/gdariawhan"
-                        className="img-fluid"
-                        src="./images/icons/icon-facebook.png"
-                        alt="facebook"
-                      />
-                    </a>
-                  </div>
-                  <div className="col-2">
-                    <a href="https://www.instagram.com/gdariawhan">
-                      <img
-                        className="img-fluid"
-                        src="./images/icons/icon-instagram.png"
-                        alt="instagram"
-                      />
-                    </a>
-                  </div>
-                  <div class="col-2">
-                    <a href="https://twitter.com/gdariawhan">
-                      <img
-                        class="img-fluid"
-                        src="./images/icons/icon-twitter.png"
-                        alt="twitter"
-                      />
-                    </a>
-                  </div>
-                  <div className="col-2">
-                    <a href="https://gmail.com">
-                      <img
-                        className="img-fluid"
-                        src="./images/icons/icon-mail.png"
-                        alt="mail"
-                      />
-                    </a>
-                  </div>
-                  <div className="col-2">
-                    <a href="https://www.twitch.tv/gdariawhan">
-                      <img
-                        className="img-fluid"
-                        src="./images/icons/icon-twitch.png"
-                        alt="twitch"
-                      />
-                    </a>
-                  </div>
+            {social.media.map((media) => {
+              return (
+                <div className="col-2">
+                  <a href={media.href}>
+                    <img
+                      className="img-fluid"
+                      src={media.imgSrc}
+                      alt={media.imgalt}
+                    />
+                  </a>
                 </div>
-              </div>
-            </div>
-            <div className="ol-lg-2 col-md-3 p-4">
-              <p>Copyright Binar 2022</p>
-              <a className="footer-logo" href="index.html"></a>
-            </div>
+              );
+            })}
           </div>
         </div>
-      </footer>
+      </React.Fragment>
+    );
+  };
+
+  copyright = (copyright) => {
+    return (
+      <React.Fragment>
+        <p>{copyright.title}</p>
+        <a className="footer-logo" href={copyright.url}></a>
+      </React.Fragment>
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <footer>
+          <div className="container mt-5">
+            <div className="row">
+              <div className="col-lg-2 col-md-3 p-4">
+                {this.contact(this.state.contact)}
+              </div>
+              <div className="ol-lg-2 col-md-3 p-4">
+                {this.navigation(this.state.navigation)}
+              </div>
+              <div className="ol-lg-2 col-md-3 p-4">
+                {this.socialMedia(this.state.social)}
+              </div>
+              <div className="ol-lg-2 col-md-3 p-4">
+                {this.copyright(this.state.copyright)}
+              </div>
+            </div>
+          </div>
+        </footer>
+      </>
     );
   }
 }
