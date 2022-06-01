2c3548327292fc55624ccd1e1315a1d201d84e29
I Gede Ariawan Eka Putra <74646042+ariawhan@users.noreply.github.com>
2022-06-02 01:20:00 +0800

    Update index.jsx

--- src/pages/registration/components/login/index.jsx
+++ src/pages/registration/components/login/index.jsx
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
-import { GoogleLogin } from "react-google-login";
-class Login extends Component {
-  state = { clientId: this.props.clientId };
-  responseGoogle = (response) => {
-    if (response.Ca !== undefined) {
-      const data = { status: true, message: response };
-      this.props.onLoginDataHandler(data);
-    } else {
-      const data = { status: false, message: response };
-      this.props.onLoginDataHandler(data);
-    }
-  };
-  render() {
-    return (
-      <div class="card">
-        <div class="card-body mt-3 mb-3">
-          <h4 class="card-title">Register to Binar Cars Rental</h4>
-          <p class="card-text">We just accept registration from google Oauth</p>
-          <GoogleLogin
-            clientId={this.state.clientId}
-            buttonText="Login with google"
-            onSuccess={this.responseGoogle}
-            onFailure={this.responseGoogle}
-            cookiePolicy={"single_host_origin"}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+// import React, { Component } from "react";
+// import { GoogleLogin } from "react-google-login";
+// class Login extends Component {
+//   state = { clientId: this.props.clientId };
+//   responseGoogle = (response) => {
+//     if (response.Ca !== undefined) {
+//       const data = { status: true, message: response };
+//       this.props.onLoginDataHandler(data);
+//     } else {
+//       const data = { status: false, message: response };
+//       this.props.onLoginDataHandler(data);
+//     }
+//   };
+//   render() {
+//     return (
+//       <div class="card">
+//         <div class="card-body mt-3 mb-3">
+//           <h4 class="card-title">Register to Binar Cars Rental</h4>
+//           <p class="card-text">We just accept registration from google Oauth</p>
+//           <GoogleLogin
+//             clientId={this.state.clientId}
+//             buttonText="Login with google"
+//             onSuccess={this.responseGoogle}
+//             onFailure={this.responseGoogle}
+//             cookiePolicy={"single_host_origin"}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default Login;
+// export default Login;
