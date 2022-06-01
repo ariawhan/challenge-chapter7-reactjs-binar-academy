fb48cb5b02755fcaf6b82acc2412fb0949ac61bb
I Gede Ariawan Eka Putra <74646042+ariawhan@users.noreply.github.com>
2022-06-02 01:26:26 +0800

    Update index.jsx

--- src/pages/registration/components/logout/index.jsx
+++ src/pages/registration/components/logout/index.jsx
@@ -1,34 +1,34 @@
-// import React, { Component } from "react";
-// import { GoogleLogout } from "react-google-login";
+import React, { Component } from "react";
+import { GoogleLogout } from "react-google-login";
 
-// class Logout extends Component {
-//   state = {
-//     clientId: this.props.clientId,
-//     email: this.props.email,
-//     name: this.props.name,
-//     googleId: this.props.googleId,
-//   };
+class Logout extends Component {
+  state = {
+    clientId: this.props.clientId,
+    email: this.props.email,
+    name: this.props.name,
+    googleId: this.props.googleId,
+  };
 
-//   onSuccess = () => {
-//     this.props.onLogoutDataHandler();
-//   };
+  onSuccess = () => {
+    this.props.onLogoutDataHandler();
+  };
 
-//   render() {
-//     return (
-//       <div class="card">
-//         <div class="card-body mt-3 mb-3">
-//           <h4 class="card-title">Welcome {this.state.name}</h4>
-//           <p class="card-text">Your Email : {this.state.email}</p>
-//           <p class="card-text">Your GoogleId : {this.state.googleId}</p>
-//           <GoogleLogout
-//             clientId={this.state.clientId}
-//             buttonText="Logout"
-//             onLogoutSuccess={this.onSuccess}
-//           />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div class="card">
+        <div class="card-body mt-3 mb-3">
+          <h4 class="card-title">Welcome {this.state.name}</h4>
+          <p class="card-text">Your Email : {this.state.email}</p>
+          <p class="card-text">Your GoogleId : {this.state.googleId}</p>
+          <GoogleLogout
+            clientId={this.state.clientId}
+            buttonText="Logout"
+            onLogoutSuccess={this.onSuccess}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
-// export default Logout;
+export default Logout;
